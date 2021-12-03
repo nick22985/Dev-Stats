8f1d2f1f6584a8f094af88cf324cf68121401447
nick22985 <nick22985@hotmail.com>
2021-12-03 21:13:55 +1000

    Initial setup Basic api calls made

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+import { WakaTimeClient, RANGE } from 'wakatime-client';
+import * as fs from 'fs';
+import * as dotenv from 'dotenv';
+import { DateTime } from "luxon";
+dotenv.config();
+
+let wakaaTimeApiKey = process.env.WAKATIMEAPIKEY
+
 async function start() {
 	console.log("start")
+	const client = await new WakaTimeClient(wakaaTimeApiKey);
+	const myUserDetails = await client.getMe();
+	const stats = await client.getMyStats({ range: RANGE.LAST_7_DAYS });
+	console.log(stats.data)
 }
 
 start()
