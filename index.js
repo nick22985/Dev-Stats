21e1af314140d3892b2d477e2cbb64d26c752ea3
nick22985 <nick22985@hotmail.com>
2021-12-07 15:31:15 +1000

    Github data objects created and returned to `index.js`

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import * as fs from 'fs';
 import * as dotenv from 'dotenv';
-
+import chalk from 'chalk';
 import { DateTime } from "luxon"; // Not needed maybe?
 
 import {getWakaStats} from './lib/waka.js'
 import {getGithubStats} from './lib/github.js'
-
-
+import {formatDoc} from './lib/fomratting.js'
 
 dotenv.config();
 
@@ -23,9 +22,13 @@ async function start() {
 		if(wakatime_api_key == undefined) throw "No WakaTime Api Key Provided"
 		if(gh_token == undefined) throw "No Github Token Provided"
 		console.log("start")
+		console.log(chalk.white.bold("WakaStats"))
 		devStats.wakatime = await getWakaStats(wakatime_api_key)
+		console.log("Github Stats")
 		devStats.github = await getGithubStats(devStats.wakatime.timezone, gh_token)
-		console.log(devStats)
+		console.log("Api Calls Completed")
+		let formatted = formatDoc(devStats)
+		console.log(formatted)
 	}
 	catch (e){console.log(e)}
 
