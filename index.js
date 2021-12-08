a858b4f3fb8e8221f4601ef0be0c529ac2b87ef2
nick22985 <nick22985@hotmail.com>
2021-12-09 00:25:58 +1000

    StaticData, Formating

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-import * as fs from 'fs';
+import fs, { write } from 'fs';
 import * as dotenv from 'dotenv';
 import chalk from 'chalk';
 import { DateTime, Settings } from "luxon"; // Not needed maybe?
-
 import {getWakaStats} from './lib/waka.js'
 import {getGithubStats} from './lib/github.js'
 import {formatDoc} from './lib/fomratting.js'
+import { stringify } from 'querystring';
+
+import staticDevStats from './data/devStats.json'
 
 dotenv.config();
+
+let dev = true;
+let staticCapture = false;
 
 
 let wakatime_api_key = process.env.WAKATIME_API_KEY;
@@ -24,21 +29,38 @@ async function start() {
 		if(gh_token == undefined) throw "No Github Token Provided"
 		console.log("start")
 		console.log(chalk.white.bold("WakaStats"))
-		devStats.wakatime = await getWakaStats(wakatime_api_key)
-		console.log("Github Stats")
-		devStats.github = await getGithubStats(devStats.wakatime.timezone, gh_token)
-		console.log("Api Calls Completed")
+		if(dev) {
+			devStats = staticDevStats
+		} else {
+			devStats.wakatime = await getWakaStats(wakatime_api_key)
+			console.log("Github Stats")
+			devStats.github = await getGithubStats(devStats.wakatime.timezone, gh_token)
+			console.log("Api Calls Completed")
+			if(staticCapture && !dev && devStats.github && devStats.wakatime) {
+				writeFile("./data/devStats.json", JSON.stringify(devStats))
+			}
+		}
+
 		Settings.defaultZoneName = devStats.wakatime.timezone;
 		devStats.currentDate = DateTime.local()
 		// console.log(devStats.currentDate)
 		let formatted = await formatDoc(devStats)
 		console.log(formatted)
+
 	}
 	catch (e){console.log(e)}
-
-
 }
 
+
+function writeFile(filename, content) {
+	fs.writeFile(filename, content, err => {
+		if (err) {
+		  console.error(err)
+		  return
+		}
+		//file written successfully
+	  })
+}
 
 
 start()
