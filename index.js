b4c5f8106c17dbcd4bc0287c1b245709229517a8
nick22985 <nick22985@hotmail.com>
2021-12-09 19:59:56 +1000

    Update readme file with new content

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-import fs from 'fs';
 import * as dotenv from 'dotenv';
 import chalk from 'chalk';
 import { DateTime, Settings } from "luxon"; // Not needed maybe?
 import {getWakaStats} from './lib/waka.js'
-import {getGithubStats} from './lib/github.js'
+import {getGithubStats, updateReadme} from './lib/github.js'
 import {formatDoc} from './lib/fomratting.js'
 import { stringify } from 'querystring';
 
@@ -11,31 +10,30 @@ import staticDevStats from './data/devStats.json'
 
 dotenv.config();
 
-let dev = true;
-let staticCapture = false;
-
-
 let wakatime_api_key = process.env.WAKATIME_API_KEY;
 let gh_token = process.env.GH_TOKEN;
 
 let settings = {
-	showShortInfo: true,
-	showCommits: true,
-	showDayOfWeek: true,
-	showBreakDown: true,
-	showTimeZone: true,
-	showEditors: true,
-	showLanguages: true,
-	showOS: true,
-	showHosts: true,
-	showLanguagePerRepo: true,
-	showProjects: true,
+	showShortInfo: process.env.SHOW_SHORT_INFO,
+	showCommits: process.env.SHOW_COMITS,
+	showDayOfWeek: process.env.SHOW_DAY_OF_WEEK,
+	showBreakDown: process.env.SHOW_BREAK_DOWN,
+	showTimeZone: process.env.SHOW_TIMEZONE,
+	showEditors: process.env.SHOW_EDITORS,
+	showLanguages: process.env.SHOW_LANGUAGES,
+	showOS: process.env.SHOW_OS,
+	showHosts: process.env.SHOW_HOSTS,
+	showLanguagePerRepo: process.env.SHOW_LANGUAGE_PER_REPO,
+	showProjects: process.env.SHOW_PROJECTS,
 	excludeProjects: process.env.EXCLUDE_PROJECT,
 	graph: [
 		'█░',
 		25
 	],
-	rowLength: 80,
+	entryPoints: {
+		start: "<!--START_SECTION:devStats-->",
+		end: "<!--END_SECTION:devStats-->",
+	},
 }
 
 async function start() {
@@ -44,47 +42,27 @@ async function start() {
 		wakatime:{},
 		currentDate: undefined,
 	}
+
 	try {
 		if(wakatime_api_key == undefined) throw "No WakaTime Api Key Provided"
 		if(gh_token == undefined) throw "No Github Token Provided"
 		console.log("start")
 		console.log(chalk.white.bold("WakaStats"))
-		if(dev) {
-			devStats = staticDevStats
-		} else {
-			devStats.wakatime = await getWakaStats(wakatime_api_key)
-			console.log("Github Stats")
-			devStats.github = await getGithubStats(devStats.wakatime.timezone, gh_token)
-			console.log("Api Calls Completed")
-			if(staticCapture && !dev && devStats.github && devStats.wakatime) {
-				writeFile("./data/devStats.json", JSON.stringify(devStats))
-			}
-		}
-
+		devStats.wakatime = await getWakaStats(wakatime_api_key)
+		console.log("Github Stats")
+		devStats.github = await getGithubStats(devStats.wakatime.timezone, gh_token)
+		console.log("Api Calls Completed")
 		Settings.defaultZoneName = devStats.wakatime.timezone;
 		devStats.currentDate = DateTime.local()
 		if(settings.excludeProjects) {
 			settings.excludeProjects = settings.excludeProjects.split(', ')
-			console.log(settings.excludeProjects)
 		}
 		let formatted = await formatDoc(devStats, settings)
-		writeFile('./format.md', formatted)
 		console.log(formatted)
+		updateReadme(gh_token, devStats.github.userInfo, settings.entryPoints, formatted)
 	}
 	catch (e){console.log(e)}
 }
-
-
-function writeFile(filename, content) {
-	fs.writeFile(filename, content, err => {
-		if (err) {
-		  console.error(err)
-		  return
-		}
-		//file written successfully
-	  })
-}
-
 
 start()
 
