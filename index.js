c91c041dd13cec330bb8a3d260dfcef6d2a28ed3
nick22985 <nick22985@hotmail.com>
2021-12-14 16:36:27 +1000

    Formatting

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import * as dotenv from 'dotenv';
-import chalk from 'chalk';
-import { DateTime, Settings } from "luxon"; // Not needed maybe?
-import {getWakaStats} from './lib/waka.js'
-import {getGithubStats, updateReadme} from './lib/github.js'
-import {formatDoc} from './lib/fomratting.js'
-import { stringify } from 'querystring';
+import * as dotenv from "dotenv";
+import chalk from "chalk";
+import {DateTime, Settings} from "luxon"; // Not needed maybe?
+import {getWakaStats} from "./lib/waka.js";
+import {getGithubStats, updateReadme} from "./lib/github.js";
+import {formatDoc} from "./lib/fomratting.js";
+import {stringify} from "querystring";
 
 dotenv.config();
 
@@ -24,44 +24,40 @@ let settings = {
 	showLanguagePerRepo: process.env.SHOW_LANGUAGE_PER_REPO,
 	showProjects: process.env.SHOW_PROJECTS,
 	excludeProjects: process.env.EXCLUDE_PROJECT,
-	graph: [
-		'█░',
-		25
-	],
+	graph: ["█░", 25],
 	entryPoints: {
 		start: "<!--START_SECTION:devStats-->",
 		end: "<!--END_SECTION:devStats-->",
 	},
-}
+};
 
 async function start() {
 	let devStats = {
 		github: {},
-		wakatime:{},
+		wakatime: {},
 		currentDate: undefined,
-	}
+	};
 
 	try {
-		if(wakatime_api_key == undefined) throw "No WakaTime Api Key Provided"
-		if(gh_token == undefined) throw "No Github Token Provided"
-		console.log("start")
-		console.log(chalk.white.bold("WakaStats"))
-		devStats.wakatime = await getWakaStats(wakatime_api_key)
-		console.log("Github Stats")
-		devStats.github = await getGithubStats(devStats.wakatime.timezone, gh_token)
-		console.log("Api Calls Completed")
+		if (wakatime_api_key == undefined) throw "No WakaTime Api Key Provided";
+		if (gh_token == undefined) throw "No Github Token Provided";
+		console.log("start");
+		console.log(chalk.white.bold("WakaStats"));
+		devStats.wakatime = await getWakaStats(wakatime_api_key);
+		console.log("Github Stats");
+		devStats.github = await getGithubStats(devStats.wakatime.timezone, gh_token);
+		console.log("Api Calls Completed");
 		Settings.defaultZoneName = devStats.wakatime.timezone;
-		devStats.currentDate = DateTime.local()
-		if(settings.excludeProjects) {
-			settings.excludeProjects = settings.excludeProjects.split(', ')
+		devStats.currentDate = DateTime.local();
+		if (settings.excludeProjects) {
+			settings.excludeProjects = settings.excludeProjects.split(", ");
 		}
-		let formatted = await formatDoc(devStats, settings)
-		console.log(formatted)
-		updateReadme(gh_token, devStats.github.userInfo, settings.entryPoints, formatted)
+		let formatted = await formatDoc(devStats, settings);
+		console.log(formatted);
+		updateReadme(gh_token, devStats.github.userInfo, settings.entryPoints, formatted);
+	} catch (e) {
+		console.log(e);
 	}
-	catch (e){console.log(e)}
 }
 
-start()
-
-
+start();
